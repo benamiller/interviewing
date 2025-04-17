9d27481f251064bbc1ec660541657961f31d33b7
benamiller <mrbenamiller@gmail.com>
2025-04-17 11:12:38 +0100

    Adds SinglyLinkedList class for linked list stuff

--- easy/objects.py
+++ easy/objects.py
@@ -34,3 +34,32 @@ class BinarySearchTree:
                         break
                 else:
                     break
+
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
+
+
+def print_singly_linked_list(node, sep):
+    while node:
+        print(str(node.data), end=sep)
+
+        node = node.next
