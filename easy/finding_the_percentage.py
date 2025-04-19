e356f2be8f70f69d3d85c897c3538f5665dcc162
benamiller <mrbenamiller@gmail.com>
2025-04-19 08:54:59 +0000

    Adds scores for each person

--- easy/finding_the_percentage.py
+++ easy/finding_the_percentage.py
@@ -5,4 +5,8 @@ if __name__ == "__main__":
     
     people_scores = {person: [] for person in people}
     
+    for person in people:
+        scores_for_person = list(map(int, input().strip().split()))
+        people_scores[person] += scores_for_person
+
     print(people_scores)
