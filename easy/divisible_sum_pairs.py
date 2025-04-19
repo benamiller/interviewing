20425449d1b81dbfc26fbb4d5bf9b5019cb90730
benamiller <mrbenamiller@gmail.com>
2025-04-19 08:41:51 +0000

    Adds divisible sum pairs solution from Pixel terminal

--- easy/divisible_sum_pairs.py
+++ easy/divisible_sum_pairs.py
@@ -1,3 +1,16 @@
+def divisibleSumPairs(arr, k):
+    n = len(arr)
+
+    num_pairs = 0
+
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            i_value = arr[i]
+            j_value = arr[j]
+            if (i_value + j_value) % k == 0:
+                num_pairs += 1
+
+    return num_pairs
 
 
 if __name__ == "__main__":
