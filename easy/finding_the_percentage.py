747a06e38d58eb2ea9b1658727bdc07f45a43aea
benamiller <mrbenamiller@gmail.com>
2025-04-21 13:56:49 +0100

    Appease the LPS that I didn't have on the Pixel 6a

--- easy/finding_the_percentage.py
+++ easy/finding_the_percentage.py
@@ -2,9 +2,9 @@
 
 if __name__ == "__main__":
     people = list(map(str, input().strip().split()))
-    
+
     people_scores = {person: [] for person in people}
-    
+
     for person in people:
         scores_for_person = list(map(int, input().strip().split()))
         people_scores[person] += scores_for_person
