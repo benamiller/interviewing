2decc8da5841924e89316245f7ccb6d145b5d464
benamiller <mrbenamiller@gmail.com>
2025-04-22 19:26:29 +0100

    Adds easy solution to pangrams

--- easy/pangrams.py
+++ easy/pangrams.py
@@ -1,3 +1,17 @@
+def pangrams(s):
+    letters_seen = {}
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    for letter in s.lower():
+        letters_seen[letter] = True
+
+    for letter in alphabet:
+        if letter not in letters_seen:
+            return "not pangram"
+
+    return "pangram"
+
+
 if __name__ == "__main__":
     s = input()
 
