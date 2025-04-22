2931f2fe163a3b7af8d4912ee93fa99dd3319f80
benamiller <mrbenamiller@gmail.com>
2025-04-22 18:26:26 +0100

    Adds input gathering for diag diff

---
+++ easy/diagonal_difference.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    print(diagonalDifference(arr))
