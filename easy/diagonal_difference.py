54e8e6062933d5144dc844ab64ac3dfa73ef6136
benamiller <mrbenamiller@gmail.com>
2025-04-22 18:29:55 +0100

    Adds easy solution for diagonal difference

--- easy/diagonal_difference.py
+++ easy/diagonal_difference.py
@@ -1,3 +1,19 @@
+def diagonalDifference(arr):
+    top_left_to_bot_right = 0
+    top_right_to_bot_left = 0
+
+    top_left_col = 0
+    top_right_col = len(arr[0]) - 1
+
+    for row in range(0, len(arr)):
+        top_left_to_bot_right += arr[row][top_left_col]
+        top_right_to_bot_left += arr[row][top_right_col]
+        top_left_col += 1
+        top_right_col -= 1
+
+    return abs(top_left_to_bot_right - top_right_to_bot_left)
+
+
 if __name__ == "__main__":
     n = int(input().strip())
 
