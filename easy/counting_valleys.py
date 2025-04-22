eb8266325c8c1fabc7fd51b758bb923c40055aa0
benamiller <mrbenamiller@gmail.com>
2025-04-22 19:15:45 +0100

    Adds easy solution for counting valleys

--- easy/counting_valleys.py
+++ easy/counting_valleys.py
@@ -1,3 +1,16 @@
+def countingValleys(path):
+    altitude = 0
+    valleys = 0
+
+    for step in path:
+        if step == "D":
+            altitude -= 1
+        if step == "U":
+            altitude += 1
+            if altitude == 0:
+                valleys += 1
+
+    return valleys
 
 
 if __name__ == "__main__":
