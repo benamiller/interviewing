76e4f37660be2c2bc4fb02d32b8e42c5eb913bc8
benamiller <mrbenamiller@gmail.com>
2025-04-23 09:35:17 +0100

    Adds easy solution for counting sort

--- easy/counting_sort_one.py
+++ easy/counting_sort_one.py
@@ -1,3 +1,12 @@
+def countingSort(arr):
+    counts = [0 for _ in range(100)]
+
+    for num in arr:
+        counts[num] += 1
+
+    return counts
+
+
 if __name__ == "__main__":
     arr = list(map(int, input().rstrip().split()))
 
