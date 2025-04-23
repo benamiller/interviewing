4335d868af95f28b04bedde127ef5264a62aa4d3
benamiller <mrbenamiller@gmail.com>
2025-04-23 11:42:04 +0100

    Adds easy permuting two arrays solution

--- easy/permuting_two_arrays.py
+++ easy/permuting_two_arrays.py
@@ -1,3 +1,12 @@
+def twoArrays(k, A, B):
+    a = sorted(A)
+    b = sorted(B, reverse=True)
+
+    for i in range(len(a)):
+        if a[i] + b[i] < k:
+            return "NO"
+
+    return "YES"
 
 
 if __name__ == "__main__":
