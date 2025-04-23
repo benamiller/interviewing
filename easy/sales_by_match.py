8fd48c875d54bd0a35b9303ea962439bcae47846
benamiller <mrbenamiller@gmail.com>
2025-04-23 12:10:43 +0100

    Adds solution to easy sock matching problem

--- easy/sales_by_match.py
+++ easy/sales_by_match.py
@@ -1,4 +1,15 @@
+def sockMerchant(ar):
+    sock_frequencies = {}
+    num_pairs = 0
 
+    for sock in ar:
+        sock_frequencies[sock] = sock_frequencies.get(sock, 0) + 1
+
+    for sock, frequency in sock_frequencies.items():
+        if frequency >= 2:
+            num_pairs += frequency // 2
+
+    return num_pairs
 
 
 if __name__ == "__main__":
