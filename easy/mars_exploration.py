27169a3592759a6667f82b35ac2146bd554a3427
benamiller <mrbenamiller@gmail.com>
2025-04-23 09:45:14 +0100

    Adds easy mars exploration solution

--- easy/mars_exploration.py
+++ easy/mars_exploration.py
@@ -1,3 +1,19 @@
+def marsExploration(s):
+    difference = 0
+
+    for i in range(0, len(s) - 2, 3):
+        substring = s[i:i+3]
+
+        if substring[0] != "S":
+            difference += 1
+        if substring[1] != "O":
+            difference += 1
+        if substring[2] != "S":
+            difference += 1
+
+    return difference
+
+
 if __name__ == "__main__":
     s = input()
 
