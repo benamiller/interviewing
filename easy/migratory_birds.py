87a2176e1157b3762a035e829642218d6f5aedc2
benamiller <mrbenamiller@gmail.com>
2025-04-23 12:28:08 +0100

    Adds easy migratory birds solution

--- easy/migratory_birds.py
+++ easy/migratory_birds.py
@@ -1,4 +1,21 @@
+def migratoryBirds(arr):
+    bird_frequencies = {}
 
+    for bird in arr:
+        bird_frequencies[bird] = bird_frequencies.get(bird, 0) + 1
+
+    highest_frequency = 0
+    highest_frequency_bird_id = None
+
+    for bird_id, frequency in bird_frequencies.items():
+        if frequency > highest_frequency:
+            highest_frequency = frequency
+            highest_frequency_bird_id = bird_id
+        elif frequency == highest_frequency:
+            if bird_id < highest_frequency_bird_id:
+                highest_frequency_bird_id = bird_id
+
+    return highest_frequency_bird_id
 
 
 if __name__ == "__main__":
