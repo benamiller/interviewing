b115aa42bfd1ec6d6daa8a25c483bf04643e8600
benamiller <mrbenamiller@gmail.com>
2025-04-23 11:52:40 +0100

    Adds non-edge-case-handling solution

---
+++ easy/subbarray_division_two.py
@@ -0,0 +1,17 @@
+def birthday(s, d, m):
+    running_sum = 0
+    number_of_ways = 0
+
+    for i in range(m - 1):
+        running_sum += s[i]
+
+    for i in range(m - 1, len(s)):
+        running_sum += s[i]
+        if running_sum == d:
+            number_of_ways += 1
+        running_sum -= s[i - m + 1]
+
+    return number_of_ways
+
+
+if __name__ == "__main__":
