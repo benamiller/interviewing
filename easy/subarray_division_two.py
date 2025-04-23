db20458d476dabd2af97fc0f0ab57d3992f91305
benamiller <mrbenamiller@gmail.com>
2025-04-23 11:57:46 +0100

    Adds edge-case-handling solution which is a little easier to understand at a glance?

--- easy/subarray_division_two.py
+++ easy/subarray_division_two.py
@@ -14,10 +14,32 @@ def birthday(s, d, m):
     return number_of_ways
 
 
+def birthdayEdgeCase(s, d, m):
+    running_sum = 0
+    number_of_ways = 0
+
+    for i in range(m):
+        running_sum += s[i]
+
+    if running_sum == d:
+        number_of_ways += 1
+
+    for i in range(m, len(s)):
+        running_sum = running_sum - s[i - m] + s[i]
+        if running_sum == d:
+            number_of_ways += 1
+
+    return number_of_ways
+
+
 if __name__ == "__main__":
     s = list(map(int, input().rstrip().split()))
 
     d = int(input())
     m = int(input())
 
-    print(birthday(s, d, m))
+    non_edge_case_answer = birthday(s, d, m)
+    edge_case_answer = birthdayEdgeCase(s, d, m)
+    assert non_edge_case_answer == edge_case_answer
+
+    print(non_edge_case_answer)
