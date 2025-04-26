a0a63585d1ef02e4256bb690d9d91db0a381bb3d
benamiller <mrbenamiller@gmail.com>
2025-04-26 16:48:49 +0100

    Adds solution tad higher in memory potentially, but no second loop

--- easy/closest_numbers.py
+++ easy/closest_numbers.py
@@ -24,6 +24,26 @@ def closestNumbers(arr):
     return result
 
 
+def closestNumbersAlt(arr):
+    result = []
+
+    arr = sorted(arr)
+    min_difference = float('inf')
+
+    for i in range(0, len(arr) - 1):
+        first = arr[i]
+        second = arr[i + 1]
+        difference = abs(first - second)
+
+        if difference < min_difference:
+            min_difference = difference
+            result = [first, second]
+        elif difference == min_difference:
+            result += [first, second]
+
+    return result
+
+
 if __name__ == "__main__":
     arr = list(map(int, input().rstrip().split()))
 
