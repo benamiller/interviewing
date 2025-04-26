733a2ed05e238581c9f01657c057005008957590
benamiller <mrbenamiller@gmail.com>
2025-04-26 16:42:24 +0100

    Adds solution that is sus

--- easy/closest_numbers.py
+++ easy/closest_numbers.py
@@ -1,3 +1,27 @@
+def closestNumbers(arr):
+    result = []
+
+    arr = sorted(arr)
+    min_difference = float('inf')
+
+    for i in range(0, len(arr) - 1):
+        first = arr[i]
+        second = arr[i + 1]
+        difference = abs(first - second)
+
+    print(min_difference)
+    for i in range(0, len(arr) - 1):
+        first = arr[i]
+        second = arr[i + 1]
+        difference = abs(first - second)
+
+        if difference == min_difference:
+            result.append(first)
+            result.append(second)
+
+    return result
+
+
 if __name__ == "__main__":
     arr = list(map(int, input().rstrip().split()))
 
