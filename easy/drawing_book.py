3ee8c3326583765e508cbcb1b0a33ca3401b7d43
benamiller <mrbenamiller@gmail.com>
2025-05-21 15:40:52 +0100

    Implementation

--- easy/drawing_book.py
+++ easy/drawing_book.py
@@ -1,3 +1,14 @@
+def pageCount(n, p):
+    n = n + 1 if n % 2 == 0 else n
+    mid_point = n // 2
+
+    # if greater than mid_point, start from end
+    if p > mid_point:
+        return (n - p) // 2
+
+    else:
+        p = p + 1 if p % 2 == 0 else p
+        return (p - 1) // 2
 
 
 if __name__ == "__main__":
